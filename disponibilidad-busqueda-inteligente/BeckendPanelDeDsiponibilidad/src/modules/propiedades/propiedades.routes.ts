e025ecfcd67c7d2c44300c13b2deb83ea10f808e
Fabricioanchundia <alex.anchundia3n@gmail.com>
2025-12-11 13:36:04 -0500

    Beckend levantado

--- disponibilidad-busqueda-inteligente/BeckendPanelDeDsiponibilidad/src/modules/propiedades/propiedades.routes.ts
+++ disponibilidad-busqueda-inteligente/BeckendPanelDeDsiponibilidad/src/modules/propiedades/propiedades.routes.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
-import { obtenerDisponibles, cambiarEstado } from "./propiedades.controller";
+import { obtenerDisponibles } from "./propiedades.controller";
 
 export const propiedadesRouter = Router();
 
-// GET para obtener propiedades disponibles
 propiedadesRouter.get("/", obtenerDisponibles);
-
-// PATCH o PUT para cambiar el estado
-propiedadesRouter.patch("/:id/estado", cambiarEstado);
