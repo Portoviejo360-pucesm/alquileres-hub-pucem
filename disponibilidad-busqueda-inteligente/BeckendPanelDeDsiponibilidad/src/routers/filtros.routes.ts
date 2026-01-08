7e5cd57dee9344a602dd8f5a08b70ac1d93b0e1f
SrChaoz <mateorubencho@gmail.com>
2026-01-08 02:08:38 -0500

    chore: limpiar archivos obsoletos y actualizar submódulos
    - Eliminar carpeta BeckendPanelDeDsiponibilidad (nombre incorrecto)
    - Eliminar documentación obsoleta (GIT_PUSH_GUIDE, GIT_REMOTE_SETUP)
    - Actualizar referencia del submódulo frontend con últimos cambios

--- disponibilidad-busqueda-inteligente/BeckendPanelDeDsiponibilidad/src/routers/filtros.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { getFiltrarPropiedades } from "../controllers/filtros.controller";
-
-export const filtrosRouter = Router();
-
-filtrosRouter.get("/propiedades", getFiltrarPropiedades);
